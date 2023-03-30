784ddf7df6b7c8750e06a4f46fd5e3fd232acd19
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-30 11:33:48 -0500

    normalize.css updated

--- js/app.js
+++ js/app.js
@@ -1,33 +1,36 @@
+// Use strict mode to enforce better coding practices
 'use strict';
 console.log('file connected');
 
-// Global Variables
-
+// Declare global variables for the elements we'll be working with
 let linkResults = document.getElementById('link');
 let voteSection = document.getElementById('vote');
 
-
+// Create and configure a new 'h2' element for the vote section title
 let voteSectionTitle = document.createElement('h2');
 voteSectionTitle.className = 'vote__title';
 voteSectionTitle.textContent = 'Vote On Our Products';
+// Append the vote section title to the vote section element
 voteSection.appendChild(voteSectionTitle);
 
-
+// Create and configure a new paragraph element for the product section description
 let productSectionDescription = document.createElement('p');
 productSectionDescription.className = 'vote__description';
 productSectionDescription.textContent = 'Vote for our next innovation! Click on the images below and help us decide which product to create next';
+// Append the product section description to the vote section element
 voteSection.appendChild(productSectionDescription);
 
-// Defined the total number of times the user can vote and the current round the user is on.
+// Define the total number of times the user can vote and the current round the user is on
 let totalRounds = 10;
 let currentRound = 0;
 
-// Creates a paragraph element to display the remainding number of votes.
+// Create and configure a new paragraph element to display the remaining number of votes
 let remainingVotesDisplay = document.createElement('p');
 remainingVotesDisplay.className = 'vote__remaining-votes';
+// Append the remaining votes display to the vote section element
 voteSection.appendChild(remainingVotesDisplay);
 
-// Constructor function called "Product".
+// Constructor function called "Product" to create new product instances
 function Product(name, src) {
   this.name = name;
   this.src = src;
@@ -35,7 +38,7 @@ function Product(name, src) {
   this.shown = 0;
 }
 
-// Array of Product instances representing a product with a name and image source.
+// Array of Product instances, each representing a product with a name and image source
 let productImages = [
   new Product('Bike', 'images/bike.png'),
   new Product('Car', 'images/car.png'),
@@ -54,79 +57,75 @@ let productImages = [
 ];
 
 
-
-// Function to generate a random number based on the length of the productImages array.
+// Function to generate a random number based on the length of the productImages array
 function getRandomNum() {
   return Math.floor(Math.random() * productImages.length);
 }
 
-
-// Function to update the text content of the remainingVotesDisplay
+// Function to update the text content of the remainingVotesDisplay element
 function updateRemainingVotesDisplay() {
-  remainingVotesDisplay.textContent = `You have ${totalRounds - currentRound} votes remaining.`;
+  remainingVotesDisplay.textContent = `You have ${ totalRounds - currentRound } votes remaining.`;
 }
 
-// Function to display three random products from the productImages array.
+// This function displays three random products from the productImages array.
 function displayProducts() {
-  // array to store the indexes of selected products.
+
+  // An array to store the indexes of selected products.
   let indexes = [];
 
-  // Generate three random numbers to add to the indexes array.
+  // This loop generates three random numbers to add to the indexes array.
   while (indexes.length < 3) {
-    let randomNum = getRandomNum();
+    let randomNum = getRandomNum(); // calling getRandomNum function to generate a random number.
 
-    /* if randomNum does not exist in the array, index() returns -1, which means indexOf(randomNum) === 1 is true.
-    If randomNum already exists in the indexes array, indexOf(randomNum) === -1 will be false, and the loop will continue to generate new numbers until it finds three unique ones.
-    */
+    // if randomNum does not exist in the indexes array, index() returns -1, which means indexOf(randomNum) === -1 is true.
+    // If randomNum already exists in the indexes array, indexOf(randomNum) === -1 will be false, and the loop will continue to generate new numbers until it finds three unique ones.
     if (indexes.indexOf(randomNum) === -1) {
       indexes.push(randomNum);
     }
+
   }
-  // Empty array to store the selected products.
+
+  // An empty array to store the selected products.
   let selectedProducts = [];
 
-  // Loop through the indexes array and add the corresponding Product instances to the selectedProducts array.
-
+  // This loop goes through the indexes array and adds the corresponding Product instances to the selectedProducts array.
   for (let i = 0; i < indexes.length; i++) {
     selectedProducts.push(productImages[indexes[i]]);
   }
 
-  // Increment the clicks property of each selected product.
+  // This loop increments the clicks and shown property of each selected product.
   for (let i = 0; i < selectedProducts.length; i++) {
     selectedProducts[i].clicks++;
     selectedProducts[i].shown++;
   }
 
-
+  // Creating a new div element with class name 'vote__images' and adding it to the voteSection element.
   let productSectionImages = document.createElement('div');
   productSectionImages.className = 'vote__images';
   voteSection.appendChild(productSectionImages);
 
-  // Loop through the selectedProducts array and create a new HTML element'img' for each product image.
+  // This loop goes through the selectedProducts array and creates a new HTML 'img' element for each product image.
   for (let i = 0; i < selectedProducts.length; i++) {
     let img = document.createElement('img');
     img.className = 'vote__image';
-    img.src = selectedProducts[i].src;
-    img.alt = selectedProducts[i].name;
+    img.src = selectedProducts[i].src; // Setting the source attribute of the 'img' element to the source of the selected product image.
+    img.alt = selectedProducts[i].name; // Setting the alt attribute of the 'img' element to the name of the selected product image.
     productSectionImages.appendChild(img);
-
-    // Increment the shown property of each selected product.
-    for (let i = 0; i < selectedProducts.length; i++) {
-      selectedProducts[i].shown++;}
 
   }
 }
 
-// Call the displayProducts function to display the initial set of product images.
+// Calling the displayProducts function to display the initial set of product images.
 displayProducts();
 
 
-// Event handler function that is called when the user clicks on a product image.
+// This is an event handler function that is called when the user clicks on a product image.
 function handleImageClick(event) {
-// Loop through the productImages array and find the Product instance that matches the image that was clicked on by checking if the image source URL contains the source URL of the current product image in the loop.
+
+  // This loop goes through the productImages array and finds the Product instance that matches the image that was clicked on by checking if the image source URL contains the source URL of the current product image in the loop.
   for (let i = 0; i < productImages.length; i++) {
     if (event.target.src.includes(productImages[i].src)) {
-      // Increment the clicks prperty of the matched Product instance by 1.
+      // If the matching Product instance is found, increment the clicks property of that Product instance by 1.
       productImages[i].clicks++;
       break;
     }
@@ -135,33 +134,37 @@ function handleImageClick(event) {
   // Remove the product images from the page by removing the last child element of the voteSection element.
   voteSection.removeChild(voteSection.lastElementChild);
 
-  // Increment the current round number and update the remainding votes display.
+  // Increment the current round number and update the remaining votes display.
   currentRound++;
   updateRemainingVotesDisplay();
 
-  // If there are remaining voting rounds, display a new set of product images by calling the displayProducts function. Otherwise, remove the event listener from the voteSection element and display a "view results" button by creating the displayResults function when clicked.
+  // If there are remaining voting rounds, display a new set of product images by calling the displayProducts function.
+  // Otherwise, remove the event listener from the voteSection element and display a "view results" button by creating the displayResults function when clicked.
   if (currentRound < totalRounds) {
     displayProducts();
   } else {
     voteSection.removeEventListener('click', handleImageClick);
 
-
+    // Create a new button element with class name 'results__view-button', text content 'View Results', and an event listener that calls the displayResults function.
     let viewResultsButton = document.createElement('button');
     viewResultsButton.className = 'results__view-button';
     viewResultsButton.textContent = 'View Results';
     viewResultsButton.addEventListener('click', displayResults);
+    // Add the new button element to the voteSection element.
     voteSection.appendChild(viewResultsButton);
+
   }
 }
 
-// Function to display the voting results.
+// This is a function to display the voting results.
 function displayResults() {
-  // Get the canvas element from the HTML.
+
+  // Get the canvas element with ID 'resultsChart' from the HTML.
   let resultsCanvas = document.getElementById('resultsChart');
   // Get the 2D context of the canvas element.
   let ctx = resultsCanvas.getContext('2d');
 
-  // Sort the productImages array in descending order of clicks.
+  // Sort the productImages array in descending order of clicks and assign it to a new variable sortedProducts.
   let sortedProducts = productImages.slice().sort(function (a, b) {
     return b.clicks - a.clicks;
   });
@@ -171,31 +174,32 @@ function displayResults() {
   let productClicks = [];
   let productShown = [];
 
-
-
-  // Loop through the sortedProducts array and populate the three arrays with the corresponding values.
+  // This loop goes through the sortedProducts array and populates the three arrays with the corresponding values.
   for (let i = 0; i < sortedProducts.length; i++) {
     let product = sortedProducts[i];
     productNames.push(product.name);
     productClicks.push(product.clicks);
     productShown.push(product.shown);
   }
+
+
+
+  // This Chart object is a bar chart that shows the number of votes and number of times each product was shown.
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: productNames,
+      labels: productNames, // The labels of the x-axis are the product names.
       datasets: [{
-        label: 'Votes',
-        data: productClicks,
-        backgroundColor: 'rgba(65, 131, 215, 0.8)',
-        borderColor: 'rgba(255, 255, 255, 1)',
+        label: 'Votes', // The label of the first dataset is 'Votes'.
+        data: productClicks, // The data for the first dataset is the number of clicks for each product.
+        backgroundColor: 'rgba(65, 131, 215, 0.8)', // The background color for the first dataset is blue.
+        borderColor: 'rgba(255, 255, 255, 1)', // The border color for the first dataset is white.
         borderWidth: 1
       }, {
-        label: 'Shown',
-        data: productShown,
-        backgroundColor: 'rgba(26, 55, 78, 0.6)',
-        borderColor: 'rgba(179, 206, 224, 1)',
-
+        label: 'Shown', // The label of the second dataset is 'Shown'.
+        data: productShown, // The data for the second dataset is the number of times each product was shown.
+        backgroundColor: 'rgba(26, 55, 78, 0.6)', // The background color for the second dataset is dark blue.
+        borderColor: 'rgba(179, 206, 224, 1)', // The border color for the second dataset is light blue.
         borderWidth: 1
       }]
     },
@@ -237,25 +241,21 @@ function displayResults() {
     }
   });
 
-
-
-
-
-
-  // Add an event listener to the form button.
+  // Get the form button element with ID 'button'.
   let saveResultButton = document.getElementById('button');
+  // Add an event listener to the form button that calls the saveResult function.
   saveResultButton.addEventListener('click', saveResult);
 
-
-showForm();
+  // Call the showForm function to display the form.
+  showForm();
 }
 
 
-// Add a click event listener to the voteSection element, which calls the handleImageClick function when a product image is clicked.
+// Add a click event listener to the voteSection element that calls the handleImageClick function when a product image is clicked.
 voteSection.addEventListener('click', handleImageClick);
 
-// Function to save the result.
-function saveResult(event){
+// This function saves the result.
+function saveResult(event) {
   // Prevent the form from submitting and refreshing the page.
   event.preventDefault();
 
@@ -265,8 +265,8 @@ function saveResult(event){
   // Get the date from the input field with id 'resultDate'.
   let resultDate = document.getElementById('resultDate').value;
 
-  //check if both name and date are provided, otherwise show an alert.
-  if(!userName || !resultDate){
+  // Check if both name and date are provided, otherwise show an alert.
+  if (!userName || !resultDate) {
     alert('Both name and date fields are required to save the result.');
     return;
   }
@@ -278,7 +278,7 @@ function saveResult(event){
   let savedResults = JSON.parse(localStorage.getItem('savedResults')) || [];
 
   // Push the new result to the savedResults array.
-  savedResults.push({name: userName, date: resultDate, imageURL: chartImageURL});
+  savedResults.push({ name: userName, date: resultDate, imageURL: chartImageURL });
 
   // Save the updated savedResults array back to local storage.
   localStorage.setItem('savedResults', JSON.stringify(savedResults));
@@ -292,10 +292,9 @@ function saveResult(event){
   resultsLink.className = 'link__link';
   resultsLink.textContent = 'Go to Results page';
   linkResults.appendChild(resultsLink);
-
 }
 
-// Function to remove the 'hidde' attribute and display the form.
+// This function removes the 'hidden' attribute and displays the form.
 function showForm() {
   let form = document.getElementById('form');
   form.removeAttribute('hidden');
--- js/results.js
+++ js/results.js
@@ -1,90 +1,83 @@
-/* eslint-disable indent */
 // Function called when the DOM is fully loaded.
 function init() {
   displaySavedResults();
   document.getElementById('clear__results-button').addEventListener('click', clearAllSavedResults);
-
 }
 
 // Function to display the saved results from local storage or display a message if there are no saved results.
 function displaySavedResults() {
+  // Get the HTML element to display the saved results.
+  let resultsList = document.getElementById('resultsList');
 
-    // Get the HTML element to display the saved results.
-    let resultsList = document.getElementById('resultsList');
+  // Retrieve saved results from local storage.
+  let savedResults = getSavedResults();
 
-    // Retrieve saved results from local storage.
-    let savedResults = getSavedResults();
-
-    // If there are no saved results, display a message.
-    if (savedResults.length === 0) {
-        displayNoResultsMessage(resultsList);
-    } else {
-
-        // Sort the savedResults array in ascending order based on the user's name using the localeCompare() function.
-        savedResults.sort(function (a, b) {
-            return a.name.localeCompare(b.name);
-        });
-
-        // Loop through each saved result and display it.
-        savedResults.forEach(function (result) {
-            displayResult(resultsList, result);
-        });
-    }
+  // If there are no saved results, display a message.
+  if (savedResults.length === 0) {
+    displayNoResultsMessage(resultsList);
+  } else {
+    // Sort the savedResults array in ascending order based on the user's name using the localeCompare() function.
+    savedResults.sort(function (a, b) {
+      return a.name.localeCompare(b.name);
+    });
+    // Loop through each saved result and display it.
+    savedResults.forEach(function (result) {
+      displayResult(resultsList, result);
+    });
+  }
 }
-
 
 // Function to display a message if there are no saved results to display.
-function displayNoResultsMessage(resultsList){
-    // Create a p element for the message.
-    let noResltsMessage = document.createElement('p');
-    noResltsMessage.textContent = 'No Saved Results to Display at This Time';
-    noResltsMessage.className = 'results_no-results-message';
-    resultsList.appendChild(noResltsMessage);
+function displayNoResultsMessage(resultsList) {
+  // Create a p element for the message.
+  let noResltsMessage = document.createElement('p');
+  noResltsMessage.textContent = 'No Saved Results to Display at This Time';
+  noResltsMessage.className = 'results_no-results-message';
+  resultsList.appendChild(noResltsMessage);
 }
 
-
 // Function to retrieve the saved results from local storage.
-function getSavedResults(){
-    return JSON.parse(localStorage.getItem('savedResults')) || [];
+function getSavedResults() {
+  // Retrieve saved results from local storage and return an empty array if there are no saved results.
+  return JSON.parse(localStorage.getItem('savedResults')) || [];
 }
 
 // Function to display a single saved result.
-function displayResult(resultsList, result){
-    // Div to style and display the result.
-    let resultsDisplay = document.createElement('div');
-    resultsDisplay.className = 'results__display';
+function displayResult(resultsList, result) {
+  // Create a div to style and display the result.
+  let resultsDisplay = document.createElement('div');
+  resultsDisplay.className = 'results__display';
 
-    // Title for the saved result with the user's name and date.
-    // Change date format to mm/dd/yyyy
-    let formattedDate = new Intl.DateTimeFormat('en-US', {month: '2-digit', day: '2-digit', year: 'numeric'}).format(new Date(result.date));
+  // Title for the saved result with the user's name and date.
+  // Change date format to mm/dd/yyyy
+  let formattedDate = new Intl.DateTimeFormat('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }).format(new Date(result.date));
 
+  // Create an h3 element for the title.
+  let resultTitle = document.createElement('h3');
+  resultTitle.textContent = `${ result.name } - ${ formattedDate }`;
+  resultTitle.className = 'results__display-title';
+  resultsDisplay.appendChild(resultTitle);
 
-    let resultTitle = document.createElement('h3');
-    resultTitle.textContent = `${result.name} - ${formattedDate}`;
-    resultTitle.className = 'results__display-title';
-    resultsDisplay.appendChild(resultTitle);
+  // Create an img element for the saved result.
+  let resultImage = document.createElement('img');
+  resultImage.src = result.imageURL;
+  resultImage.alt = `${ result.name } - ${ result.date }`;
+  resultImage.className = 'results__display-image';
+  resultsDisplay.appendChild(resultImage);
 
-
-    // Image element with the saved result.
-    let resultImage = document.createElement('img');
-    resultImage.src = result.imageURL;
-    resultImage.alt = `${result.name} - ${result.date}`;
-    resultImage.className = 'results__display-image';
-    resultsDisplay.appendChild(resultImage);
-
-    // Append the resultsDisplay to the results list.
-    resultsList.appendChild(resultsDisplay);
+  // Append the resultsDisplay to the results list.
+  resultsList.appendChild(resultsDisplay);
 }
 
 // Function to clear all saved results using button.
-function clearAllSavedResults(){
-    // Remove saved results from local storage.
-    localStorage.removeItem('savedResults');
-    // clear results section
-    let resultsList = document.getElementById('resultsList');
-    resultsList.innerHTML = '';
-    // Display the no saved results message.
-    displayNoResultsMessage(resultsList);
+function clearAllSavedResults() {
+  // Remove saved results from local storage.
+  localStorage.removeItem('savedResults');
+  // Clear the results section.
+  let resultsList = document.getElementById('resultsList');
+  resultsList.innerHTML = '';
+  // Display the no saved results message.
+  displayNoResultsMessage(resultsList);
 }
 
 // Link the init function to the DOMContentLoaded event.
